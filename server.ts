6b4ae8038afd027cf55946e5cfbcfb7b0eb7d92c
Anh The Dinh Thanh <quangcaodongyen@gmail.com>
2026-08-14 12:39:10 +0700

    Add files via upload

--- server.ts
+++ server.ts
@@ -201,4 +201,9 @@ async function startServer() {
   });
 }
 
-startServer();
+// Only start the server if not running on Vercel
+if (!process.env.VERCEL) {
+  startServer();
+}
+
+export default app;
